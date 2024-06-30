0f673034a1c62fe75a1e7848a84415e9035f629a
Sahil873 <sahilp0873@gmail.com>
2024-06-30 19:18:33 +0530

    completed week 6 2-useCallback

--- week-6/2-use-callback/src/components/Assignment1.jsx
+++ week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,31 +1,41 @@
-import { useState } from "react";
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
+import { memo, useCallback, useState } from "react";
 
-// Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
+// Create a counter component with increment and decrement functions. Pass these functions to
+// a child component which has buttons to perform the increment and decrement actions.
+// Use useCallback to ensure that these functions are not recreated on every render.
 
 export function Assignment1() {
-    const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-    // Your code starts here
-    function handleIncrement() {
+  // Your code starts here
+  const handleIncrement = useCallback(() => {
+    setCount1((currentCount) => currentCount + 1);
+  }, []);
 
-    }
+  const handleDecrement = useCallback(() => {
+    setCount2((currentCount) => currentCount - 1);
+  }, []);
+  // Your code ends here
 
-    function handleDecrement() {
-        
-    }
-    // Your code ends here
-
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
-        </div>
-    );
-};
-
-const CounterButtons = ({ onIncrement, onDecrement }) => (
+  return (
     <div>
-        <button onClick={onIncrement}>Increment</button>
-        <button onClick={onDecrement}>Decrement</button>
+      <p>Count: {count1}</p>
+      <p>Count: {count2}</p>
+      <CounterButtons fn={handleIncrement} />
+      <br />
+      <br />
+      <CounterButtons fn={handleDecrement} />
     </div>
-);
+  );
+}
+
+const CounterButtons = memo(({ fn }) => {
+  return (
+    <div>
+      <button onClick={fn}>change count</button>
+    </div>
+  );
+});
