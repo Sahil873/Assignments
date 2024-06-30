68d6382d099f8a19eb1459c18fdc69c5e4e75221
Sahil873 <sahilp0873@gmail.com>
2024-06-30 20:06:20 +0530

    completed Week 6 3-useRef

--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,19 +1,22 @@
-import React, { useState, useCallback } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useCallback, useReducer, useRef } from "react";
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
 export function Assignment2() {
-    const [, forceRender] = useState(0);
+  const [, forceRender] = useState(0);
+  const numOfRenders = useRef(0); // reference remains the same across renders but value can be changed
+  numOfRenders.current = numOfRenders.current + 1;
 
-    const handleReRender = () => {
-        // Update state to force re-render
-        forceRender(Math.random());
-    };
+  const handleReRender = () => {
+    // Update state to force re-render
+    forceRender(Math.random());
+  };
 
-    return (
-        <div>
-            <p>This component has rendered {0} times.</p>
-            <button onClick={handleReRender}>Force Re-render</button>
-        </div>
-    );
-};
+  return (
+    <div>
+      <p>This component has rendered {numOfRenders.current} times.</p>
+      <button onClick={handleReRender}>Force Re-render</button>
+    </div>
+  );
+}
