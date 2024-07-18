f2aea7ff142081b3e6876a02787ac1ee807785f4
Sahil873 <sahilp0873@gmail.com>
2024-07-18 14:36:02 +0530

    updated en

--- week-10/2-prisma-simple/jest.config.js
+++ week-10/2-prisma-simple/jest.config.js
@@ -1,14 +1,15 @@
-// jest.config.js
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    // Optionally, set up paths to match your project structure
-    roots: ['<rootDir>/src'],
-    // Transform settings if you have custom needs, but this is optional since ts-jest is preset
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
+  preset: 'ts-jest', // Change this to 'ts-jest' if you're using TypeScript
+  testEnvironment: 'node',
+  roots: ['<rootDir>/dist'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest', // Change this to handle TypeScript files if needed
+    '^.+\\.js$': 'babel-jest', // Use 'babel-jest' for JavaScript files
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json', // Optional: specify your tsconfig file
     },
-    // Module file extensions for importing
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  };
-  
+  },
+};
